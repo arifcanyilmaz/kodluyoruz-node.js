001c62133d9fc8353a920a175c7cbfe443157394
Arifcan Yılmaz <wisemann1917@gmail.com>
2024-10-16 15:02:56 +0300

    fixed

---
+++ project1_PCAT/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo name',
+    desc: 'Photo description',
+  };
+  res.send(photo);
+});
+// server'ın çalışması için listen metodunu yaz
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port}'de baslatildi..`);
+});
